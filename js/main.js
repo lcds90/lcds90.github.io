11fea9388268b4d17145239546a75fbfd5f9f40d
Leonardo Santos <lcds90@gmail.com>
2021-05-13 18:24:12 -0300

    fonts

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const tl = gsap.timeline();
+
+
+function introAnimation(){
+    tl.from("#sectionMenu", {y: -1000, duration: 5})
+    tl.from("h1", {rotate:20, x: 1000, duration: 2}, "-=2.5")
+    tl.from("h2", {x: -1000, duration: 3},  "-=2")
+    tl.from("#about-me__text", {scale: 0,opacity:0, rotate: 180, duration:1})
+}
+
+introAnimation();
