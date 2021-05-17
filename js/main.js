c4a7785db95e338fdcc4fa908e3806000cb9096c
Leonardo Santos <lcds90@gmail.com>
2021-05-17 13:54:16 -0300

    photo and view changes

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ function introAnimation(){
     tl.from("h1", {rotate:20, x: 1000, duration: 2}, "-=2.5")
     tl.from("h2", {x: -1000, duration: 3},  "-=2")
     tl.from("#about-me__text", {scale: 0,opacity:0, rotate: 180, duration:1})
+    tl.from("")
 }
 
 introAnimation();
