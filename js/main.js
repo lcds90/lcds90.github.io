212b4f6ed5d0594203fad2478cf16c3a53bc7e0b
Leonardo Santos <lcds90@gmail.com>
2021-05-17 15:13:11 -0300

    photo changes in div

--- js/main.js
+++ js/main.js
@@ -2,11 +2,12 @@ const tl = gsap.timeline();
 
 
 function introAnimation(){
-    tl.from("#sectionMenu", {y: -1000, duration: 5})
-    tl.from("h1", {rotate:20, x: 1000, duration: 2}, "-=2.5")
-    tl.from("h2", {x: -1000, duration: 3},  "-=2")
-    tl.from("#about-me__text", {scale: 0,opacity:0, rotate: 180, duration:1})
-    tl.from("")
+    tl.from("#sectionMenu", {y: -1000, duration: 5, ease: "bounce"})
+    tl.from("h1", {rotate:20, x: 1000, duration: 2, ease: "linear"}, "-=2.5")
+    tl.from("h2", {x: -1000, duration: 3, ease: "power2"},  "-=2")
+    tl.from("#about-me", {x: -1000, duration: 2}, "-=5")
+    tl.from(".about-me__text", {x: -100, zIndex: -1,opacity:0, duration:1, delay: 2}, "-=4")
+    tl.from(".photo-circle", {zIndex: 2,opacity: 0, duration: 2, ease: "power"}, "-=3.5")
 }
 
 introAnimation();
